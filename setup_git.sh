77b48e5f439b54ee71343c38945dd613b25a62ae
yupingso <yupingso@google.com>
2019-06-17 16:45:34 +0800

    Add git setup script

---
+++ setup_git.sh
@@ -0,0 +1,9 @@
+# alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+# whitespace for diff
+git config --global color.diff.whitespace "red reverse"
+git config --global diff.wsErrorHighlight all
