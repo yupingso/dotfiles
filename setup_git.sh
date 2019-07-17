6fadd061c535b5ac290d20406b20f10a11b63540
Yu-Ping Wu <yupingso@gmail.com>
2019-07-17 10:46:21 +0800

    Add alias 'git cia'

--- setup_git.sh
+++ setup_git.sh
@@ -2,6 +2,7 @@
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
+git config --global alias.cia 'commit --amend'
 git config --global alias.st status
 
 # whitespace for diff
