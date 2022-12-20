e7fded77248defc975680129e6fb71ceb88f5b31
Yu-Ping Wu <yupingso@chromium.org>
2022-12-20 12:21:01 +0800

    Add git alias for cherry-pick

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -3,6 +3,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.cia 'commit --amend'
+git config --global alias.cp cherry-pick
 git config --global alias.st status
 git config --global alias.su 'submodule update --checkout'
 git config --global alias.log1 'log --oneline'
